d6d0d9469432156d1ac8cbb5da9f7abdd62146f8
RNViththagan <viththagan.rn@gmail.com>
2026-06-16 15:08:48 +0530

    Add skill-reminder hooks

---
+++ hooks/bal-skill-mark.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+input=$(cat)
+skill=$(echo "$input" | jq -r '.tool_input.skill // .tool_input.name // ""')
+[[ "$skill" == "ballerina" ]] || exit 0
+
+session_id=$(echo "$input" | jq -r '.session_id // ""')
+[[ -z "$session_id" ]] && exit 0
+
+tmpdir="${TMPDIR:-/tmp}"
+MARKER="${tmpdir%/}/.ballerina-skill-${session_id}"
+touch "$MARKER"
+exit 0
